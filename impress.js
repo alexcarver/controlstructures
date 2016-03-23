34dd3778b912f69e6262c42cb8b1b8b2808094be
alexcarver <alexcarver12@gmail.com>
2016-03-23 11:03:03 -0400

    Create impress.js

---
+++ impress.js
@@ -0,0 +1,11 @@
+<!doctype html>
+
+<h1>Cloud Application</h1>
+<img src="http://www.crn.com/ckfinder/userfiles/images/crn/images/cloud_apps400.jpg" align="middle" width="200">
+<!-- 
+    how do you center the image?
+-->
+<p> This presentation is in regards to Cloud Applications </p>
+<!-- 
+    can you center text
+-->
